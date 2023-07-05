ff9bf582998228c90d6f3e6a468500f95a5055e2
EC2 Default User <ec2-user@ip-10-0-136-123.eu-west-2.compute.internal>
2023-07-05 12:30:31 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+import { Icon, Menu, Dropdown } from 'semantic-ui-react';
+
+function AuthStateApp() {
+  document.title = 'Travel Deals';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className='App'>
+          <Menu fixed='top' color='teal' inverted>
+            <Menu.Menu>
+              <Menu.Item header href='/'><Icon name='globe' />Travel Deals</Menu.Item>
+            </Menu.Menu>
+            <Menu.Menu position='right'>
+              <Dropdown item simple text='User Name'>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => alert('Log-out')}><Icon name='power off' />Log Out</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Menu.Menu>
+          </Menu>
+        </div>
   );
 }
-
-export default App;
+export default AuthStateApp;
